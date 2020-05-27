1d9e424662a6d3478596c588e9324e22280e3bec
vankatwijk <hendrikus.hpvk@gmail.com>
2020-05-27 03:15:53 -0800

    copy followers function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from time import sleep
-from secrets import pw
+from secrets import gituser
+from secrets import gitpw
+from secrets import gitCopyFollowers
 
 
 class GithubBot:
@@ -9,7 +11,7 @@ class GithubBot:
         self.copyfollowers = copyfollowers
 
         # START LOGIN PROCESS
-        self.driver = webdriver.Chrome("/chromedriver")
+        self.driver = webdriver.Chrome("/home/hkatwijk/repo/GitHub-Follow-Bot/chromedriver")
         self.driver.get("https://github.com/login")
         # wait for the page to load the DOM
         sleep(2)
@@ -25,9 +27,25 @@ class GithubBot:
         # END LOGIN PROCESS
 
     def copy_followers(self):
+        #here we go to our target and make a list of all there followers
         #go to profile
-        
+        self.driver.get("https://github.com/{}?tab=followers".format(self.copyfollowers))
+        sleep(6)
 
+        #get all the followers from the first page
+        users = self.driver.find_elements_by_xpath("//a[@data-hovercard-type='user']")
+        temp = []
+
+        for follower in users:
+            temp.append(follower.get_attribute("href"))
+
+        list_set = set(temp) 
+        self.followers = (list(list_set))
+        print(followers)
+
+    def match_follower():
+        #here we will determin if this is a good person to follow from the above list
 
 
 my_bot = GithubBot(gituser,gitpw,gitCopyFollowers)
+my_bot.copy_followers()
